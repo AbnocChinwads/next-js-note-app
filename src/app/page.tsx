f33996b433afa493914acffeed7f87f2659c199e
AbnocChinwads <rnemo@hotmail.co.uk>
2026-06-19 13:40:19 +0100

    refactor: enhance note management with edit and delete functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,59 @@ import { useState } from "react";
 
 export default function Home() {
   const [note, setNote] = useState("");
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState<string[]>([]);
+  const [editIndex, setEditIndex] = useState<number | null>(null);
 
-  function addNote() {
+  function handleSubmit() {
     if (!note.trim()) return;
 
-    setNotes([...notes, note]);
+    if (editIndex !== null) {
+      const updatedNotes = notes.map((item, index) =>
+        index === editIndex ? note : item,
+      );
+
+      setNotes(updatedNotes);
+      setEditIndex(null);
+    } else {
+      setNotes([...notes, note]);
+    }
+
     setNote("");
+  }
+
+  function deleteNote(indexToRemove: number) {
+    setNotes(
+      notes.filter((_, index) => {
+        return index !== indexToRemove;
+      }),
+    );
   }
 
   return (
     <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <input type="text" className="border resize p-4" value={note} onChange={(e) => setNote(e.target.value)} />
-      <button className="border p-2" onClick={addNote}>
-        Add note
+      <input
+        type="text"
+        className="border resize p-2 my-2"
+        value={note}
+        onChange={(e) => setNote(e.target.value)}
+      />
+      <button className="border p-1" onClick={handleSubmit}>
+        {editIndex !== null ? "Save" : "Add Note"}
       </button>
-      <ol>
+      <ol className="p-2">
         {notes.map((item, index) => (
-          <li key={index} className="p-2">{item}</li>
+          <li key={index}>
+            {item}
+            <button className="ml-1"
+              onClick={() => {
+                setNote(item);
+                setEditIndex(index);
+              }}
+            >
+              Edit
+            </button>
+            <button className="ml-1" onClick={() => deleteNote(index)}>Delete</button>
+          </li>
         ))}
       </ol>
     </main>
